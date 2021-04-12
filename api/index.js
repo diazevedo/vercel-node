3a2c32670489732209cb1b1a4d2204a4114e95d9
Diego Azevedo <dapptn@hotmail.com>
2021-04-13 00:38:17 +1000

    test api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   const name = req.query.name || "stranger";
-  res.status(200).json({ res: "ok", req: req });
+  res.status(200).json({ res: "ok", req: req.body });
 };
