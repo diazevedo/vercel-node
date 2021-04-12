fee3ca4f9ac923329b10cd00ff928a9c588ce1cb
Diego Azevedo <dapptn@hotmail.com>
2021-04-13 01:19:54 +1000

    test api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 module.exports = (req, res) => {
-  if (req.method === "POST") res.status(200).json({ message: "eh post" });
+  if (req.method === "POST")
+    res.status(200).json({ message: "eh post", name: req.body.name });
   else res.status(200).json({ message: "outra coisa" });
 };
