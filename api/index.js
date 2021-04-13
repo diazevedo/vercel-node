bff5fedf9568e8e0719174f0e040bd7322c60a41
Diego Azevedo <dapptn@hotmail.com>
2021-04-13 17:07:51 +1000

    tes http

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-module.exports = (req, res) => {
-  if (req.method === "POST")
-    res.status(200).json({ message: "eh post", name: req.body.name });
-  else res.status(200).json({ message: "outra coisa" });
-};
+const http = require("http");
+
+const port = 3333;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>ss, World!</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`server running at port ${port}`);
+});
