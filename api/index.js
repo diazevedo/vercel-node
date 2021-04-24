952dec95c474c0e986d498d7e577687a74a54705
Diego Azevedo <dapptn@hotmail.com>
2021-04-24 18:39:50 +1000

    dad

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
-const app = require("express")();
-
-app.get("/api", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item`);
-});
-
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
-
-module.exports = app;
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Home Page Route"));
+app.get("/about", (req, res) => res.send("About Page Route"));
+app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+app.get("/contact", (req, res) => res.send("Contact Page Route"));
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
