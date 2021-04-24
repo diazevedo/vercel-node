7e4e156699f3160bd6ca64d7df8263005a39b3de
Diego Azevedo <dapptn@hotmail.com>
2021-04-24 18:21:02 +1000

    new route

--- api/index.js
+++ api/index.js
@@ -1,110 +1,17 @@
-import express from "express";
-import cors from "cors";
-import crypto from "crypto";
-import path from "path";
-import mongoose from "mongoose";
-import multer from "multer";
-import GridFsStorage from "multer-gridfs-storage";
-import dotenv from "dotenv";
+const express = require("express");
 
 const app = express();
-dotenv.config({ path: "./../.env" });
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
+app.get("/", (req, res) => res.send("Home Page Route"));
 
-// app.get("/", function (req, res) {
-//   res.send("Hello World!");
-// });
+app.get("/about", (req, res) => res.send("About Page Route"));
 
-app.get("/api", (req, res) => {
-  // const path = `/api/item/${v4()}`;
-  // res.setHeader("Content-Type", "text/html");
-  // res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="sdsdsd">dssdsd</a>`);
-});
+app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
 
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+app.get("/contact", (req, res) => res.send("Contact Page Route"));
 
-module.exports = app;
-// // connection
-// const conn = mongoose.createConnection(process.env.MONGO_URL, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const port = process.env.PORT || 3000;
 
-// // init gfs
-// let gfs;
-// conn.once("open", () => {
-//   gfs = new mongoose.mongo.GridFSBucket(conn.db, {
-//     bucketName: "uploads",
-//   });
-// });
-
-// // Storage
-// const storage = new GridFsStorage({
-//   url: process.env.MONGO_URL,
-//   file: (req, file) => {
-//     return new Promise((resolve, reject) => {
-//       crypto.randomBytes(16, (err, buf) => {
-//         if (err) {
-//           return reject(err);
-//         }
-
-//         const filename = buf.toString("hex") + path.extname(file.originalname);
-//         const fileInfo = {
-//           filename: filename,
-//           bucketName: "uploads",
-//         };
-//         resolve(fileInfo);
-//       });
-//     });
-//   },
-// });
-
-// const upload = multer({
-//   storage,
-// });
-
-// app.post("/api/files", upload.single("file"), (req, res) => {
-//   res.status(201).json({
-//     message: req.file,
-//   });
-// });
-
-// app.get("/api/files", (req, res) => {
-//   gfs.find().toArray((err, files) => {
-//     // check if files
-//     if (!files || files.length === 0) {
-//       return res.status(404).json({
-//         err: "no files exist",
-//       });
-//     }
-
-//     return res.json(files);
-//   });
-// });
-
-// app.get("/api/files/:filename", async (req, res) => {
-//   gfs
-//     .find({
-//       filename: req.params.filename,
-//     })
-//     .toArray((err, files) => {
-//       if (!files || files.length === 0) {
-//         return res.status(404).json({
-//           err: "no files exist",
-//         });
-//       }
-
-//       gfs.openDownloadStreamByName(req.params.filename).pipe(res);
-//     });
-// });
-
-// app.listen(process.env.PORT, () => {
-//   console.log("server started on " + process.env.PORT);
-// });
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
