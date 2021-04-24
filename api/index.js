d59f25836e360327d7268553af016a18f1462f08
Diego Azevedo <dapptn@hotmail.com>
2021-04-24 18:24:32 +1000

    new route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,16 @@
-const express = require("express");
+const app = require("express")();
+const { v4 } = require("uuid");
 
-const app = express();
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
 
-app.get("/", (req, res) => res.send("Home Page Route"));
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
-app.get("/about", (req, res) => res.send("About Page Route"));
-
-app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
-
-app.get("/contact", (req, res) => res.send("Contact Page Route"));
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () =>
-  console.log(`Server running on ${port}, http://localhost:${port}`)
-);
+module.exports = app;
